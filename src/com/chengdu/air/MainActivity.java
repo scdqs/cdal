84e7b1ea9b00807fb30e528c0e2caaddacc2c62f
androidzhaoxiaogang <18008021570@189.cn>
2013-12-09 09:21:27 +0800

    resource chechin;

--- src/com/chengdu/air/MainActivity.java
+++ src/com/chengdu/air/MainActivity.java
@@ -3,13 +3,15 @@ package com.chengdu.air;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.Window;
 
 public class MainActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.activity_home);
 	}
 
 	@Override
